9a33f1ab4f4687528a7a72aee2020b4b2de0821f
eduarfailmaster <193228@ids.upchiapas.edu.mx>
2021-07-19 16:40:46 -0500

    second commit

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('/', function(req, res, next) {
+  res.send('ACA VA LA PAGINA DE EYSSI');
+});
+
 router.get('/habitaciones', function(req, res, next) {
   res.render('Habitaciones/habitacion', { title: 'Express' });
 });
